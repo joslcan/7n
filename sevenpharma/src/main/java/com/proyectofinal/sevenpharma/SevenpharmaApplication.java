a68a0173cdec637fe2de723dc4b0bda4d00e4105
joslcan <josluicanvas@gmail.com>
2026-06-08 16:49:51 -0500

    Sube el proyecto inicial

---
+++ sevenpharma/src/main/java/com/proyectofinal/sevenpharma/SevenpharmaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.sevenpharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenpharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenpharmaApplication.class, args);
+	}
+
+}
---
+++ sevenpharma/src/test/java/com/proyectofinal/sevenpharma/SevenpharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.sevenpharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenpharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
